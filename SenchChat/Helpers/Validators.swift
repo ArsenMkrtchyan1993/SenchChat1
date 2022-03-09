bf96ef2763c13fd40b86a3b91645e2ec6cb2c1f5
Arsen <arsenn077@gmail.com>
2022-03-09 20:48:16 +0300

    work  errors in register

---
+++ SenchChat/Helpers/Validators.swift
@@ -0,0 +1,30 @@
+//
+//  Validators.swift
+//  SenchChat
+//
+//  Created by Arsen Mkrtchyan on 09.03.22.
+//
+
+import Foundation
+
+
+class Validators {
+    
+    static func isFilled(email:String?, password: String?,conformPassword:String?) -> Bool {
+        guard let password = password,
+              let email = email,
+              let conformPassword = conformPassword,
+                  password != "",
+                  conformPassword != "",
+              email != "" else { return false}
+        return true
+    }
+    static func isSimpleEmail(_ email:String) -> Bool {
+        let emailPattern = #"^\S+@\S+\.\S+$"#
+        return check(text: email, regEx: emailPattern)
+    }
+    private static func check(text: String, regEx: String) -> Bool {
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regEx)
+        return predicate.evaluate(with: text)
+    }
+}
--- SenchChat/Services/AuthService.swift
+++ SenchChat/Services/AuthService.swift
@@ -14,7 +14,12 @@ class AuthService {
     static let shared = AuthService()
     private let auth = Auth.auth()
     func login(email: String?, password:String?, completion: @escaping (Result<User,Error>) -> Void) {
-        auth.signIn(withEmail: email!, password: password!) { result, error in
+        guard let email = email,
+        let password = password else {
+            completion(.failure(AuthError.notFilled))
+            return
+        }
+    auth.signIn(withEmail: email, password: password) { result, error in
             guard let result = result else {
                 completion(.failure(error!))
                 return
@@ -25,7 +30,22 @@ class AuthService {
     }
     
     func register(email: String?, password:String?, confirmPass:String?, completion: @escaping (Result<User,Error>) -> Void) {
+        
+       
+        guard Validators.isFilled(email:email, password: password, conformPassword: confirmPass) else {
+            completion(.failure(AuthError.notFilled))
+            return
+        }
+        guard password?.lowercased() == confirmPass?.lowercased() else {
+            completion(.failure(AuthError.passwordNotMatched))
+            return
+        }
+        guard Validators.isSimpleEmail(email!) else {
+            completion(.failure(AuthError.invalidEmail))
+            return
+        }
         auth.createUser(withEmail: email!, password: password!) { result, error in
+           
             guard let result = result else {
                 completion(.failure(error!))
                 return
